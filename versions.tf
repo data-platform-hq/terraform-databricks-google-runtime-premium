d3e61550744e53751d954a22c30be274ff4042f5
Oleh Mykolaishyn <owlleg6@gmail.com>
2023-08-21 11:11:34 +0300

    fix: updated provider file

--- versions.tf
+++ versions.tf
@@ -2,17 +2,9 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-
     databricks = {
       source  = "databricks/databricks"
       version = ">= 1.19.0"
     }
-
   }
 }
-
-# provider "databricks" {
-#   alias                  = "workspace"
-#   host                   = module.databricks_ws.workspace_url
-#   google_service_account = var.google_service_account_email
-# }
