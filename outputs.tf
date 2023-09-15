b1e205e377cac7f574af10c7328917fea9c575d7
owlleg6 <owlleg6@gmail.com>
2023-09-15 12:14:53 +0300

    feat: clusters support

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,13 @@
 output "token" {
-  value       = length(var.automation_sa) != 0 ? databricks_obo_token.automation[0].token_value : null
-  description = "Databricks On-Behalf-Of Authorization Token"
+  value       = { for k, v in local.token_list : k => databricks_obo_token.this[k].token_value if local.token_list != null }
+  description = "Databricks Personal Authorization Tokens"
   sensitive   = true
+}
+
+output "clusters" {
+  value = [for param in var.clusters : {
+    name = param.cluster_name
+    id   = databricks_cluster.cluster[param.cluster_name].id
+  } if length(var.clusters) != 0]
+  description = "Provides name and unique identifier for the clusters"
 }
